a2299c0ed6a3641c3e0e94c51b77afe4192e7c76
Jaelyn Lee <leejay1020@gmail.com>
2023-08-10 10:37:51 +1200

    update design your fitness app link

--- src/components/BuildYourFitness.tsx
+++ src/components/BuildYourFitness.tsx
@@ -2,9 +2,9 @@ export default function BuildYourFitness() {
   return (
     <div className="bg-background opacity-80 rounded-md p-2 mb-2">
       <div className="sm:flex sm:items-center">
-        <h1 className=" font-semibold text-lg">Build your Fitness</h1>
+        <h1 className=" font-semibold text-lg">Design your Fitness</h1>
         <h1>
-          &nbsp; / TypeScript, React.js, Redux, SQLite [Personal projects]
+          &nbsp; / TypeScript, React.js, React Query, SQLite, Tailwind [Personal projects]
         </h1>
       </div>
       <div className="sm:flex py-2">
@@ -15,20 +15,20 @@ export default function BuildYourFitness() {
         />
         <div className="pl-5 mb-2">
           <p className="pb-2">
-            This app is currently under development. It is designed for users
-            who want to start exercising but are not yet confident in planning
-            their own workout routines. The app will generate a workout plan
-            based on user-provided information such as fitness goals and
-            preferences. Additionally, it will allow users to track changes in
-            their weight over time.
+            This is a personal project that utilizes React to create a
+            full-stack app. It's designed to help users who want to start
+            exercising, but may not be confident in planning their own workout
+            routines. The app generates a personalized workout plan based on
+            user-provided information such as fitness goals and preferences,
+            while also allowing them to track changes in their weight over time.
           </p>
           <div className="flex items-center">
-            <p>Figma link:</p>
+            <p>Project link: </p>
             <a
-              href="https://www.figma.com/file/9EtZVQj7f2OW3bV9EruC8S/Design-your-fitness?type=design&node-id=0%3A1&mode=design&t=6EJkFzNRnbo6Zlis-1"
+              href="https://github.com/jaelyn-lee/Build-your-fitness"
               className=" text-red-500 underline hover:text-yellow-400"
             >
-              click here to check the wireframe
+              https://github.com/jaelyn-lee/Build-your-fitness
             </a>
           </div>
         </div>
